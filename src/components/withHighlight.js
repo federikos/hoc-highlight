b8abaf377630242bf3bd0961bb7f61c2a14153be
federikos <federikos@bk.ru>
2019-11-08 02:45:13 +0300

    cut the code

--- src/components/withHighlight.js
+++ src/components/withHighlight.js
@@ -5,23 +5,22 @@ import Popular from './Popular';
 function withHighlight(Component) {
   return class extends React.Component {
     render() {
+      const component = <Component {...this.props} />;
       if (this.props.views >= 1000) {
         return(
           <Popular>
-            <Component {...this.props} />
+            {component}
           </Popular>
         )
       }
       if (this.props.views < 100) {
         return(
           <New>
-            <Component {...this.props} />
+            {component}
           </New>
         )
       }
-      return(
-        <Component {...this.props} />
-      )
+      return component;
     }
   }
 };
